ba0ae3e19db60fc7f112d59c23921fe4bf1432d0
Mohd-Taqi <mohammedtaqiprince234@gmail.com>
2022-12-29 00:52:56 +0530

    learn

---
+++ weatherapi/sqaure(context execution).js
@@ -0,0 +1,13 @@
+let  n=2;
+
+function square(num){
+   let ans=num*num;
+   return ans
+}
+console.clear();
+var square1=square(n);
+console.log(square1) 
+var square2= square(4)
+console.log(square2)
+
+
---
+++ weatherapi/weather.js
@@ -0,0 +1,79 @@
+import axios from "axios";
+import readlineSync from "readline-sync";
+import chalk from "chalk";
+//URL
+
+//https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+
+let apikey = "d3091d08a6160152b327f3868e0ad011";
+
+const getWeatherData = () => {
+  console.clear();
+  console.log(chalk.blue("****************************************"));
+  console.log(chalk.green("\t\tWEATHER CLI"));
+  console.log(chalk.blue("****************************************"));
+  let city = readlineSync.question("Enter the name of the city: ");
+  axios
+    .get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`
+    )
+    .then((res) => {
+      let data = res.data;
+      console.log(data.name);// here it refers to api main.name object
+      console.log(data);
+      console.log(city);
+
+      let cel = toCelcius(data.main.temp);
+      console.log(`Temperature in Celcius is ${cel.toFixed(2)} \u{00B0}C`);
+      let far = toFarhenheit(cel);
+      console.log(`Temperature in Farhenheit is ${far.toFixed(2)} \u{00B0}F`);
+     
+      let celmin = toCelcius(data.main.temp_min);
+      let farmin = toFarhenheit(celmin);
+      let celmax = toCelcius(data.main.temp_max);
+      let farmax = toFarhenheit(celmax);
+   
+      console.log(`\nMinimum temperature in celcius: ${celmin.toFixed(2)} \u{00B0}C`);
+      console.log(`Minimum temperature in farhenheit: ${farmin.toFixed(2)} \u{00B0}F`);
+      console.log(`Maximum temperature in celcius: ${celmax.toFixed(2)}\u{00B0}C`);
+      console.log(`Maximum temperature in farhenheit: ${farmax.toFixed(2)}\u{00B0}F`);
+
+      let sunrise = toUTC(data.sys.sunrise);
+      let sunset = toUTC(data.sys.sunset);
+
+    
+      console.log("\nSunrise time in UTC : " + sunrise);
+      console.log("Sunset time in UTC : " + sunset);
+
+      console.log("Humidity: " + data.main.humidity);
+
+      let zone = data.timezone
+      let localtime = toUTC(data.dt+zone);
+      let utc = toUTC(data.dt)
+      console.log(`The local time in ${city} is ${localtime}`);
+      console.log(`The UTC time is ${utc}`)
+      
+    })
+    .catch((err) => {
+    //   console.error(err.response);
+    console.error(err.response.data.message);
+    console.log("wrong")
+
+    });
+};
+
+
+
+const toCelcius = (temp) => {
+  return temp - 273;
+};
+
+const toFarhenheit = (temp) => {
+  return temp * (9 / 5) + 32;
+};
+getWeatherData();
+
+
+function toUTC(sec) {
+  return new Date(sec * 1000).toLocaleTimeString("en-US", { timeZone: "UTC" });
+}
